54ed20c473e98f32d7044b2b413130a24bebc3ff
Sonny Gonzalez <sonnyg.email@gmail.com>
2013-11-16 12:49:11 -0600

    add User and Lesson to blueprint

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,3 +1,4 @@
+require 'faker'
 require 'machinist/active_record'
 
 # Add your blueprints here.
@@ -7,3 +8,19 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+Lesson.blueprint do
+  body  { Faker::Lorem.paragraph(5) }
+  title { Faker::Lorem.sentence.titleize }
+end
+
+Lesson.blueprint(:public) do
+  is_public { true }
+end
+
+User.blueprint do
+  email           { Faker::Internet.email }
+  password        { "test1234" }
+  password_confirmation { object.password }
+  name            { Faker::Name.name }
+end
