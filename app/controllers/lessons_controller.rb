82781890f3443516bc5caa4d037ff0b91b256687
Matt Buck <github@matthewbuck.com>
2013-11-16 11:32:37 -0600

    Ensure lessons are created on new, and set to draft state

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,7 @@ class LessonsController < ApplicationController
   before_filter :authenticate_user!
 
   def new
-    @lesson = current_user.lessons.new
+    @lesson = current_user.lessons.create draft: true
   end
 
   def calendar
--- db/migrate/20131116072545_create_lessons.rb
+++ db/migrate/20131116072545_create_lessons.rb
@@ -2,6 +2,7 @@ class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
       t.text :body
+      t.boolean :draft, default: true
       t.references :user, index: true
       t.string :title
 
