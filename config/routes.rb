16c962dba9991cfaee46465952466845c3cd1ffc
Matt Buck <github@matthewbuck.com>
2013-11-16 03:01:47 -0600

    Add lessons menu item

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ TheDesk::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+
+  resources :lessons
 end
