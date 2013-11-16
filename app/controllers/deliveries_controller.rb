616ab1b3bdc79b23e21e593745571a6bfaf6ec1f
Kaoru <dev@kaoruk.com>
2013-11-16 13:14:03 -0600

    Adds ability to create a delivery date only if it is a valid date

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,5 +1,18 @@
 class DeliveriesController < ApplicationController
   def create
-    
+    date = Chronic.parse(params[:delivery][:date])
+
+    if date
+      delivery = Lesson.find(params[:lesson_id]).deliveries.create(delivery_params)
+      render :json => delivery.date
+    else
+      render :json => {}
+    end
+  end
+
+  private
+
+  def delivery_params
+    params.required(:delivery).permit(:date)
   end
 end
