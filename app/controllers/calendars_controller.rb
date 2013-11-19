fda7ac1cd84a89e77f6e644a182dbcbeb4f7ba4b
Kaoru <dev@kaoruk.com>
2013-11-19 14:54:59 -0600

    Beefs up cache-key to include end date from request

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,11 +1,11 @@
 class CalendarsController < ApplicationController
   def index
-    hash = (params[:user_id] + params[:start])
+    hash = (params[:user_id] + params[:start] + params[:end])
 
     respond_to do |format|
       format.html
       format.json {
-        deliveries = Rails.cache.fetch(hash, race_condition_ttl: 10) do
+        deliveries = Rails.cache.fetch(hash, expires_in: 10) do
           time_range = Time.at(params[:start].to_i)..Time.at(params[:end].to_i)
           Delivery.get_dates_for_calendar_in time_range
         end
