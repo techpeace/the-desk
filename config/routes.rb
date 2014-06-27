3b3862f75d1353d8a1bce6294fa1076346b00d50
Kaoru Kohashigawa <kaoru@massrelevance.com>
2014-06-27 11:01:59 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ TheDesk::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users do
     get "/calendar" => "calendars#show", :as => :calendar
-
-    resources :lessons, only: [:show] do
-      resources :ratings, only: [:update]
-    end
   end
 
   resources :courses, only: [:new, :create, :show, :index]
 
   resources :lessons, only: [:show, :new, :create, :update] do
     post "/comments" => "comments#create", :as => :comments
+
+    resources :ratings, only: [:update]
 
     collection do
       get 'calendar'
