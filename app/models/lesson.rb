5cc6e662c97d9012f27bd10b18e7b048a0ede6c3
Kaoru Kohashigawa <kaoru@massrelevance.com>
2014-06-27 11:02:30 -0500

    Add average rating and rescue from error

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,12 @@ class Lesson < ActiveRecord::Base
   has_many :deliveries, :dependent => :destroy
   has_many :comments, :dependent => :destroy
   has_many :ratings
+
+  def average_rating
+    begin
+      ratings.sum(:score) / ratings.size
+    rescue ZeroDivisionError => e
+      0
+    end
+  end
 end
