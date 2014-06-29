829da362298da1652c9e7db1656622805eba6a8d
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-06-29 17:18:17 -0500

    Setup relation between course and standards

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,14 @@
 class Course < ActiveRecord::Base
+  belongs_to :user
   belongs_to :standard_course
   has_many :standards, :through => :standard_course
-  belongs_to :user
   has_many :lessons
+  has_many :lesson_standards, :through => :lessons
+  has_many :assigned_standards, :source => :standard, :through => :lesson_standards
+
+  def unassigned_standards
+    standards.where.not(id: assigned_standards.pluck(:id))
+  end
 
   GRADES = %w(K 1 2 3 4 5 6 7 8 HS)
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,10 +1,12 @@
 class Lesson < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
+  has_many :lesson_standards, :dependent => :destroy
+  has_many :standards, :through => :lesson_standards
 
   has_many :deliveries, :dependent => :destroy
   has_many :comments, :dependent => :destroy
-  has_many :ratings
+  has_many :ratings, :dependent => :destroy
 
   def average_rating
     begin
