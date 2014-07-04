b4be7038c2492e4a1c57a5f85947212e9a3efc54
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-07-04 17:02:22 -0500

    Setup fe to destroy standard relations via ajax

--- app/controllers/lesson_standards_controller.rb
+++ app/controllers/lesson_standards_controller.rb
@@ -1,2 +1,8 @@
 class LessonStandardsController < ApplicationController
+  respond_to :json
+
+  def destroy
+    LessonStandard.where(standard_id: params[:id], lesson_id: params[:lesson_id]).first.try(:destroy!)
+    render :json => {}, :status => :ok
+  end
 end
